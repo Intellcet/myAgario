45f985580abb8265ae2b6258c94a99a20286a4bc
Evgeny Osipov <zheka0168@gmail.com>
2019-07-17 23:02:19 +0400

    removed offline game

--- offlane game/js/game/input.js
+++
@@ -1,9 +0,0 @@
-﻿function Input(options) {
-    
-    const move = options.move;
-    const canvas = options.canvas;
-
-    $(canvas).on('mousemove', event =>{
-        move(event.originalEvent.offsetX, event.originalEvent.offsetY);
-    });
-}
